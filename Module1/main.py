a559b7931fc4520ad5b69f85b8ad40020c444c24
nesaa-a <agnesahalimii@outlook.com>
2024-10-18 17:00:34 +0200

    added variables lists

---
+++ Module1/main.py
@@ -0,0 +1,31 @@
+emri="Agnesa"
+mosha=19
+gjinia='F'
+
+print(emri)
+print(mosha)
+print(gjinia)
+
+
+print(type(gjinia))
+
+mosha=mosha+10
+print(mosha)
+
+emri_mbiemri = emri + " Halimi" #concatined
+print(emri_mbiemri)
+
+my_list=["Agnesa", 19, "Shkolla Digjitale"]
+age=my_list[1]
+print(age)
+
+my_list.append("Studente")
+print(my_list)
+
+my_list.insert(2, "Prishtine")
+print(my_list)
+
+my_list.remove("Prishtine")
+del my_list[0]
+print(my_list)
+
